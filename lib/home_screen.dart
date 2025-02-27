737fedc7d5ee1522198c3517a00566ad09a324f5
priyanshi <sq.priyanshi23@gmail.com>
2025-02-27 09:00:11 +0530

    27-02-25

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -23,82 +23,90 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       body: SafeArea(
-          child: Form(
-        key: _formKey,
-        child: Column(children: [
-          TextFormField(
-            controller: firstNameController,
-            validator: (value) {
-              if (value != null && value.isNotEmpty) {
-                return null;
-              }
-              return "Please enter valid first name";
-            },
-            decoration: InputDecoration(
-              hintText: "First Name",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 15),
-          TextFormField(
-            controller: lastNameController,
-            validator: (value) {
-              if (value != null && value.isNotEmpty) {
-                return null;
-              }
-              return "Please enter valid last name";
-            },
-            decoration: InputDecoration(
-              hintText: "Last Name",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 50),
-          isLoading
-              ? CircularProgressIndicator()
-              : TextButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      final firstName = firstNameController.text;
-                      final lastName = lastNameController.text;
-                      final result = await service
-                          .add({"firstName": firstName, "lastName": lastName});
-                      if (result != null) {
-                        setState(() {
-                          firstNameController.clear();
-                          lastNameController.clear();
-                          isLoading = false;
-                        });
-                      } else {
-                        isLoading = false;
-                      }
-                      print("result ======= $result");
-                    }
-                  },
-                  child: Text("Add User"),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: firstNameController,
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    return null;
+                  }
+                  return "Please enter valid first name";
+                },
+                decoration: InputDecoration(
+                  hintText: "First Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
                   ),
-          TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return UserDataPage();
-                  },
-                ));
-              },
-              child: Text("Show All user"))
-        ]),
-      )),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                controller: lastNameController,
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    return null;
+                  }
+                  return "Please enter valid last name";
+                },
+                decoration: InputDecoration(
+                  hintText: "Last Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              isLoading
+                  ? CircularProgressIndicator()
+                  : TextButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          final firstName = firstNameController.text;
+                          final lastName = lastNameController.text;
+                          final result = await service.add(
+                              {"firstName": firstName, "lastName": lastName});
+                          if (result != null) {
+                            setState(() {
+                              firstNameController.clear();
+                              lastNameController.clear();
+                              isLoading = false;
+                            });
+                          } else {
+                            isLoading = false;
+                          }
+                          print("result ======= $result");
+                        }
+                      },
+                      child: Text("Add User"),
+                    ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return UserDataPage();
+                    },
+                  ));
+                },
+                child: Text("Show All user"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
